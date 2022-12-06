b4d2ad2885bb083bf52c26b22c0b6bf183a2a184
Stéphane Ghozzi <1541878-stephaneghozzi@users.noreply.gitlab.com>
2022-12-06 13:04:07 +0100

    see previous commit

--- data-raw/generate-data.R
+++ data-raw/generate-data.R
@@ -8,7 +8,6 @@ path_to_data <- "data"
 
 # WARNING!!! As of 5 December 2022: This often generates an error (Gumbel dist)!
 # sormas_testdata <- sormasdatagen::GenerateSORMASData()
-# saveRDS(sormas_testdata, paste0(path_to_data, "/", "sormas_testdata.rds"))
 
 # Export CSVs
 for (ds in names(sormas_testdata)) {
